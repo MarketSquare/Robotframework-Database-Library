43d13b8632632fbe22f3bab5b70d77c36a272a05
Franz See <franz.see@gmail.com>
2010-11-28 09:42:10 +0800

    Renamed 'connect' to 'connect_to_database' and 'disconnect' to 'disconnect_from_database'.

--- src/DatabaseLibrary/connection_manager.py
+++ src/DatabaseLibrary/connection_manager.py
@@ -24,18 +24,13 @@ class ConnectionManager(object):
         '''
         self._dbconnection = None
         
-    def connect(self, dbProvider, database, username, password):
+    def connect_to_database(self, dbProvider, database, username, password):
         if dbProvider == "postgres":
             import psycopg2
             self._dbconnection = psycopg2.connect (database=database, user=username, password=password)
         else:
             raise AssertionError("The database provider '%' is not supported" % dbProvider)
         
-    def disconnect(self):
+    def disconnect_from_database(self):
         self._dbconnection.close()
-        
-        
-        
-        
-        
         
