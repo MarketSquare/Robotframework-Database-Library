d725b3bf8afe4095a23d8b183651d2afa857931c
Franz See <franz.see@gmail.com>
2010-11-28 09:51:34 +0800

    Removed commented out code.

--- src/DatabaseLibrary/query.py
+++ src/DatabaseLibrary/query.py
@@ -18,7 +18,6 @@ class Query(object):
 
     def query(self, selectStatement):
         """Uses the input select statement to query for the values that will be returned."""
-        '''raise AssertionError("Always an error. Message: '%s'." % (assertionMessage))'''
         cur = self._dbconnection.cursor()
         cur.execute (selectStatement);
         return cur.fetchall()
