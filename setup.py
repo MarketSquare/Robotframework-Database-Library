0043090d30d80fae5c8b4772a559f536f1f0a334
schminitz <gillian@affinitic.be>
2013-02-22 10:33:49 +0100

    set personnal version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from distutils.core import setup
 import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 
-version = '0.6'
+version = '0.6-schminitz'
 
 def main():
     setup(name         = 'robotframework-databaselibrary',
