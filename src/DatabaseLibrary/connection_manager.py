0893ec084ed58aae6797055a3e3cdd0cd72159a4
Dennis Roberts <dennis@iplantcollaborative.org>
2014-11-26 13:58:30 -0700

    removed trailing whitespace

--- src/DatabaseLibrary/connection_manager.py
+++ src/DatabaseLibrary/connection_manager.py
@@ -25,90 +25,89 @@ class ConnectionManager(object):
         Initializes _dbconnection to None.
         """
         self._dbconnection = None
-        
+
     def connect_to_database(self, dbapiModuleName=None, dbName=None, dbUsername=None, dbPassword=None, dbHost=None, dbPort=None, dbConfigFile="./resources/db.cfg"):
         """
-        Loads the DB API 2.0 module given `dbapiModuleName` then uses it to 
+        Loads the DB API 2.0 module given `dbapiModuleName` then uses it to
         connect to the database using `dbName`, `dbUsername`, and `dbPassword`.
-        
+
         Optionally, you can specify a `dbConfigFile` wherein it will load the
-        default property values for `dbapiModuleName`, `dbName` `dbUsername` 
-        and `dbPassword` (note: specifying `dbapiModuleName`, `dbName` 
+        default property values for `dbapiModuleName`, `dbName` `dbUsername`
+        and `dbPassword` (note: specifying `dbapiModuleName`, `dbName`
         `dbUsername` or `dbPassword` directly will override the properties of
         the same key in `dbConfigFile`). If no `dbConfigFile` is specified, it
-        defaults to `./resources/db.cfg`. 
-        
+        defaults to `./resources/db.cfg`.
+
         The `dbConfigFile` is useful if you don't want to check into your SCM
         your database credentials.
-        
+
         Example usage:
         | # explicitly specifies all db property values |
         | Connect To Database | psycopg2 | my_db | postgres | s3cr3t | tiger.foobar.com | 5432 |
 
         | # loads all property values from default.cfg |
-        | Connect To Database | dbConfigFile=default.cfg | 
-        
+        | Connect To Database | dbConfigFile=default.cfg |
+
         | # loads all property values from ./resources/db.cfg |
-        | Connect To Database |  
-        
+        | Connect To Database |
+
         | # uses explicit `dbapiModuleName` and `dbName` but uses the `dbUsername` and `dbPassword` in 'default.cfg' |
         | Connect To Database | psycopg2 | my_db_test | dbConfigFile=default.cfg |
-        
+
         | # uses explicit `dbapiModuleName` and `dbName` but uses the `dbUsername` and `dbPassword` in './resources/db.cfg' |
-        | Connect To Database | psycopg2 | my_db_test |    
+        | Connect To Database | psycopg2 | my_db_test |
         """
-    
+
         config = ConfigParser.ConfigParser()
         config.read([dbConfigFile])
-        
+
         dbapiModuleName = dbapiModuleName or config.get('default', 'dbapiModuleName')
         dbName = dbName or config.get('default', 'dbName')
         dbUsername = dbUsername or config.get('default', 'dbUsername')
         dbPassword = dbPassword or config.get('default', 'dbPassword')
         dbHost = dbHost or config.get('default', 'dbHost') or 'localhost'
         dbPort = int(dbPort or config.get('default', 'dbPort'))
-        
+
         db_api_2 = __import__(dbapiModuleName)
         if dbapiModuleName in ["MySQLdb", "pymysql"]:
             dbPort = dbPort or 3306
             logger.debug ('Connecting using : %s.connect(db=%s, user=%s, passwd=%s, host=%s, port=%s) ' % (dbapiModuleName, dbName, dbUsername, dbPassword, dbHost, dbPort))
             self._dbconnection = db_api_2.connect (db=dbName, user=dbUsername, passwd=dbPassword, host=dbHost, port=dbPort)
         elif dbapiModuleName in ["psycopg2"]:
-            dbPort = dbPort or 5432            
+            dbPort = dbPort or 5432
             logger.debug ('Connecting using : %s.connect(database=%s, user=%s, password=%s, host=%s, port=%s) ' % (dbapiModuleName, dbName, dbUsername, dbPassword, dbHost, dbPort))
             self._dbconnection = db_api_2.connect (database=dbName, user=dbUsername, password=dbPassword, host=dbHost, port=dbPort)
         elif dbapiModuleName in ["pyodbc"]:
-            dbPort = dbPort or 1433            
+            dbPort = dbPort or 1433
             logger.debug ('Connecting using : %s.connect(DRIVER={SQL Server};SERVER=%s,%s;DATABASE=%s;UID=%s;PWD=%s)' % (dbapiModuleName,dbHost,dbPort,dbName,dbPort,dbUsername, dbPassword))
             self._dbconnection = db_api_2.connect('DRIVER={SQL Server};SERVER=%s,%s;DATABASE=%s;UID=%s;PWD=%s'%(dbHost,dbPort,dbName,dbUsername,dbPassword))
         else:
             logger.debug ('Connecting using : %s.connect(database=%s, user=%s, password=%s, host=%s, port=%s) ' % (dbapiModuleName, dbName, dbUsername, dbPassword, dbHost, dbPort))
             self._dbconnection = db_api_2.connect (database=dbName, user=dbUsername, password=dbPassword, host=dbHost, port=dbPort)
-            
+
     def connect_to_database_using_custom_params(self, dbapiModuleName=None, db_connect_string=''):
         """
-        Loads the DB API 2.0 module given `dbapiModuleName` then uses it to 
+        Loads the DB API 2.0 module given `dbapiModuleName` then uses it to
         connect to the database using the map string `db_custom_param_string`.
-        
+
         Example usage:
         | # for psycopg2 |
         | Connect To Database Using Custom Params | psycopg2 | database='my_db_test', user='postgres', password='s3cr3t', host='tiger.foobar.com', port=5432 |
-        
-        | # for JayDeBeApi | 
+
+        | # for JayDeBeApi |
         | Connect To Database Using Custom Params | JayDeBeApi | 'oracle.jdbc.driver.OracleDriver', 'my_db_test', 'system', 's3cr3t' |
         """
         db_api_2 = __import__(dbapiModuleName)
-        
+
         db_connect_string = 'db_api_2.connect(%s)' % db_connect_string
-        
+
         self._dbconnection = eval(db_connect_string)
-        
+
     def disconnect_from_database(self):
         """
         Disconnects from the database.
-        
+
         For example:
-        | Disconnect From Database | # disconnects from current connection to the database | 
+        | Disconnect From Database | # disconnects from current connection to the database |
         """
         self._dbconnection.close()
-        
--- src/DatabaseLibrary/query.py
+++ src/DatabaseLibrary/query.py
@@ -16,36 +16,36 @@ from robot.api import logger
 
 class Query(object):
     """
-    Query handles all the querying done by the Database Library. 
+    Query handles all the querying done by the Database Library.
     """
 
     def query(self, selectStatement):
         """
-        Uses the input `selectStatement` to query for the values that 
+        Uses the input `selectStatement` to query for the values that
         will be returned as a list of tuples.
-        
-        Tip: Unless you want to log all column values of the specified rows, 
-        try specifying the column names in your select statements 
+
+        Tip: Unless you want to log all column values of the specified rows,
+        try specifying the column names in your select statements
         as much as possible to prevent any unnecessary surprises with schema
-        changes and to easily see what your [] indexing is trying to retrieve 
-        (i.e. instead of `"select * from my_table"`, try 
+        changes and to easily see what your [] indexing is trying to retrieve
+        (i.e. instead of `"select * from my_table"`, try
         `"select id, col_1, col_2 from my_table"`).
-        
+
         For example, given we have a table `person` with the following data:
         | id | first_name  | last_name |
         |  1 | Franz Allan | See       |
-        
+
         When you do the following:
         | @{queryResults} | Query | select * from person |
         | Log Many | @{queryResults} |
-        
+
         You will get the following:
         [1, 'Franz Allan', 'See']
-        
+
         Also, you can do something like this:
         | ${queryResults} | Query | select first_name, last_name from person |
         | Log | ${queryResults[0][1]}, ${queryResults[0][0]} |
-        
+
         And get the following
         See, Franz Allan
         """
@@ -57,29 +57,29 @@ class Query(object):
             return allRows
         finally :
             if cur :
-                self._dbconnection.rollback() 
+                self._dbconnection.rollback()
 
     def row_count(self, selectStatement):
         """
         Uses the input `selectStatement` to query the database and returns
         the number of rows from the query.
-        
+
         For example, given we have a table `person` with the following data:
         | id | first_name  | last_name |
         |  1 | Franz Allan | See       |
         |  2 | Jerry       | Schneider |
-        
+
         When you do the following:
         | ${rowCount} | Row Count | select * from person |
         | Log | ${rowCount} |
-        
+
         You will get the following:
         2
-        
+
         Also, you can do something like this:
         | ${rowCount} | Row Count | select * from person where id = 2 |
         | Log | ${rowCount} |
-        
+
         And get the following
         1
         """
@@ -92,21 +92,21 @@ class Query(object):
             return rowCount
         finally :
             if cur :
-                self._dbconnection.rollback() 
+                self._dbconnection.rollback()
 
     def description(self, selectStatement):
         """
         Uses the input `selectStatement` to query a table in the db which
         will be used to determine the description.
-                
+
         For example, given we have a table `person` with the following data:
         | id | first_name  | last_name |
         |  1 | Franz Allan | See       |
-        
+
         When you do the following:
         | @{queryResults} | Description | select * from person |
         | Log Many | @{queryResults} |
-        
+
         You will get the following:
         [Column(name='id', type_code=1043, display_size=None, internal_size=255, precision=None, scale=None, null_ok=None)]
         [Column(name='first_name', type_code=1043, display_size=None, internal_size=255, precision=None, scale=None, null_ok=None)]
@@ -120,14 +120,14 @@ class Query(object):
             return description
         finally :
             if cur :
-                self._dbconnection.rollback() 
+                self._dbconnection.rollback()
 
     def delete_all_rows_from_table(self, tableName):
         """
         Delete all the rows within a given table.
-        
+
         For example, given we have a table `person` in a database
-        
+
         When you do the following:
         | Delete All Rows From Table | person |
 
@@ -147,13 +147,13 @@ class Query(object):
             self._dbconnection.commit()
         finally :
             if cur :
-                self._dbconnection.rollback() 
+                self._dbconnection.rollback()
 
     def execute_sql_script(self, sqlScriptFileName):
         """
-        Executes the content of the `sqlScriptFileName` as SQL commands. 
-        Useful for setting the database to a known state before running 
-        your tests, or clearing out your test data after running each a 
+        Executes the content of the `sqlScriptFileName` as SQL commands.
+        Useful for setting the database to a known state before running
+        your tests, or clearing out your test data after running each a
         test.
 
         Sample usage :
@@ -164,48 +164,48 @@ class Query(object):
         | Execute Sql Script | ${EXECDIR}${/}resources${/}DDL-teardown.sql |
 
         SQL commands are expected to be delimited by a semi-colon (';').
-        
+
         For example:
         delete from person_employee_table;
         delete from person_table;
-        delete from employee_table; 
-        
+        delete from employee_table;
+
         Also, the last SQL command can optionally omit its trailing semi-colon.
-        
+
         For example:
         delete from person_employee_table;
         delete from person_table;
         delete from employee_table
-        
+
         Given this, that means you can create spread your SQL commands in several
         lines.
-        
+
         For example:
-        delete 
+        delete
           from person_employee_table;
-        delete 
+        delete
           from person_table;
-        delete 
+        delete
           from employee_table
-        
+
         However, lines that starts with a number sign (`#`) are treated as a
         commented line. Thus, none of the contents of that line will be executed.
-        
+
         For example:
         # Delete the bridging table first...
-        delete 
+        delete
           from person_employee_table;
           # ...and then the bridged tables.
-        delete 
+        delete
           from person_table;
-        delete 
+        delete
           from employee_table
         """
         sqlScriptFile = open(sqlScriptFileName)
 
         cur = None
         try:
-            cur = self._dbconnection.cursor()        
+            cur = self._dbconnection.cursor()
             sqlStatement = ''
             for line in sqlScriptFile:
                 line = line.strip()
@@ -213,7 +213,7 @@ class Query(object):
                     continue
                 elif line.startswith('--'):
                     continue
-                
+
                 sqlFragments = line.split(';')
                 if len(sqlFragments) == 1:
                     sqlStatement += line + ' '
@@ -222,21 +222,21 @@ class Query(object):
                         sqlFragment = sqlFragment.strip()
                         if len(sqlFragment) == 0:
                             continue
-                    
+
                         sqlStatement += sqlFragment + ' '
-                        
+
                         self.__execute_sql(cur, sqlStatement)
                         sqlStatement = ''
 
-            sqlStatement = sqlStatement.strip()    
+            sqlStatement = sqlStatement.strip()
             if len(sqlStatement) != 0:
                 self.__execute_sql(cur, sqlStatement)
-                
+
             self._dbconnection.commit()
         finally:
             if cur :
                 self._dbconnection.rollback()
-                
+
     def execute_sql_string(self, sqlString):
         """
         Executes the sqlString as SQL commands.
