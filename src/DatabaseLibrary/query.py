fe8c7320604479a895ca9a5da3114b74ed71dc73
Jerry Schneider <jschneider@theabr.org>
2018-08-09 14:28:15 -0700

    fix for Row Count returns -1 when using pyodbc for MS SQL

--- src/DatabaseLibrary/query.py
+++ src/DatabaseLibrary/query.py
@@ -111,7 +111,7 @@ class Query(object):
             logger.info('Executing : Row Count  |  %s ' % selectStatement)
             self.__execute_sql(cur, selectStatement)
             data = cur.fetchall()
-            if self.db_api_module_name in ["sqlite3", "ibm_db", "ibm_db_dbi"]:
+            if self.db_api_module_name in ["sqlite3", "ibm_db", "ibm_db_dbi", "pyodbc"]:
                 rowCount = len(data)
             else:
                 rowCount = cur.rowcount
