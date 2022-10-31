634b20f92f549356fa1be8bdd1b7f8ceecbf7db1
Mateusz Kotas <mateusz.kotas@gmail.com>
2022-10-31 10:56:29 +0100

    fix wrong operator

--- src/DatabaseLibrary/connection_manager.py
+++ src/DatabaseLibrary/connection_manager.py
@@ -60,7 +60,7 @@ class ConnectionManager(object):
             logger.info(
                 f"Connecting using : databricks.sql.connect(server_hostname={dbHost}, http_path={dbHttpPath}, catalog={dbCatalog}, schema={dbSchema}) "
             )
-            self.db_api_module_name == 'databricks'
+            self.db_api_module_name = 'databricks'
             self._dbconnection = module.connect(
                 server_hostname=dbHost,
                 access_token=dbToken,
